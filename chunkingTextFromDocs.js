cb4d4929a4c6d177ba081f1fcad23cd8b1a294d7
Nerevakiin <djmatisk@yahoo.gr>
2026-07-18 15:23:19 +0300

    langchain integration and chunking

---
+++ chunkingTextFromDocs.js
@@ -0,0 +1,16 @@
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+
+// Langchain text splitter
+async function splitDocument() {
+    const response = await fetch('podcasts.txt')
+    const text = await response.text()
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 150,
+        chunkOverlap: 15
+    })
+    const output = await splitter.createDocuments([text])
+    console.log(output)
+}
+
+splitDocument()
